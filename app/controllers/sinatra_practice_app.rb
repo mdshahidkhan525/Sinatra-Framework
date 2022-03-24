00cab804b331ecd7c7817cc28ba1b721996986cd
shahid <shkhan@bestpeers.com>
2022-03-24 11:35:07 +0530

    some changes

--- app/controllers/sinatra_practice_app.rb
+++ app/controllers/sinatra_practice_app.rb
@@ -3,33 +3,48 @@ class SinatraPracticeApp < Sinatra::Base
   require 'pry'
 
   get '/' do
+    redirect to('blogs/index')
+  end
+
+  get '/blogs/index' do
     @blogs = Blog.all
     erb :"blogs/index"
   end
 
-  get '/new' do
+  get '/blogs/new' do
+    @blog = Blog.new
     erb :"blogs/new"
   end
 
-  post '/' do
-    blog = Blog.new(params["blog"])
-    if blog.save
-      redirect "/blogs/#{blog.id}"
+  post '/blogs/create' do
+    @blog = Blog.new(params["blog"])
+    if @blog.save
+      redirect to("/blogs/#{@blog.id}")
     else
       erb :"blogs/new"
     end
   end
-
+  
   get '/blogs/:id' do
     @blog = Blog.find(params[:id])
     erb :"blogs/show"
   end
 
-  put '/blogs/edit/:id' do
-    blog = Blog.find(params[:id])
-    erb :"blogs/show"
+  get '/blogs/:id/edit' do
+    @blog = Blog.find(params[:id])
+    erb :"blogs/edit"
   end
-  post '/blogs/destroy/:id' do
+
+  patch "/blogs/:id" do
+    @blog = Blog.find(params[:id])
+    if @blog.update(params.slice(:title, :content))
+      redirect to "/blogs/#{@blog.id}"
+    else
+      erb :edit # re-render the form
+    end
+  end
+
+  post '/blogs/:id/destroy' do
     blog = Blog.find(params[:id])
     blog.destroy
     redirect to("/")
